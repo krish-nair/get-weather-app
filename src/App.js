4eb34dc031d332770cd7aa2b58439dc4416b2ed4
krish-nair <krish.nair101@gmail.com>
2019-06-10 13:45:35 +1200

    Added titles component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Titles from "./components/titles";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Titles />
+      </div>
+    );
+  }
+}
+
+export default App;
