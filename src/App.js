ab1d9d67ac0619de33e0e12078860915e792fe70
krish-nair <krish.nair101@gmail.com>
2019-06-10 14:06:50 +1200

    Fetch data from openweathermap api

--- src/App.js
+++ src/App.js
@@ -2,8 +2,20 @@ import React, { Component } from "react";
 import Titles from "./components/titles";
 import Form from "./components/form";
 import Weather from "./components/weather";
+import { async } from "q";
+
+const API_KEY = "c1c34449fbcbcfc6a602baa7028303c3";
 
 class App extends Component {
+  getWeather = async () => {
+    const api_call = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=Wellington,Newzealand&appid=${API_KEY}&units=metric`
+    );
+
+    const data = api_call.json();
+    console.log(data);
+  };
+
   render() {
     return (
       <div>
