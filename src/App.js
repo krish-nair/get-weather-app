b4e0bcf6980c0e46d878400b3bf6eaec866e91f9
krish-nair <krish.nair101@gmail.com>
2019-06-10 21:13:22 +1200

    Set the states to get values and edit weather component to show data on the screen

--- src/App.js
+++ src/App.js
@@ -7,14 +7,43 @@ import { async } from "q";
 const API_KEY = "c1c34449fbcbcfc6a602baa7028303c3";
 
 class App extends Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  };
   getWeather = async e => {
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
     e.preventDefault();
     const api_call = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=Wellington,Newzealand&appid=${API_KEY}&units=metric`
+      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`
     );
 
     const data = await api_call.json();
     console.log(data);
+    if (city && country) {
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
+      });
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "Please enter the values"
+      });
+    }
   };
 
   render() {
@@ -22,7 +51,14 @@ class App extends Component {
       <div>
         <Titles />
         <Form getWeather={this.getWeather} />
-        <Weather />
+        <Weather
+          temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error={this.state.error}
+        />
       </div>
     );
   }
