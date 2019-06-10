1c6512a10fde297a6fb2ec3dfc3a51e18f868b73
krish-nair <krish.nair101@gmail.com>
2019-06-10 13:51:08 +1200

    Created form and weather componens

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import Titles from "./components/titles";
+import Form from "./components/form";
+import Weather from "./components/weather";
 
 class App extends Component {
   render() {
     return (
       <div>
         <Titles />
+        <Form />
+        <Weather />
       </div>
     );
   }
---
+++ src/components/form.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>Form components</div>
+         );
+    }
+}
+ 
+export default Form;
